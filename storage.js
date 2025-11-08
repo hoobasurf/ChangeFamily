0a42b0f1d49471b71f971e4bf10438adedef21e4
hoobasurf <brice.paneela@icloud.com>
2025-11-08 23:03:58 +0100

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,11 +1,21 @@
-import { storage } from './firebase.js';
+import { storage, db } from './firebase.js';
 import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
+// Upload image + ajouter document Firestore
 export async function uploadImage(file, userId) {
   const name = `${Date.now()}.jpg`;
   const storageRef = ref(storage, `snaps/${userId}/${name}`);
-
+  
   await uploadBytes(storageRef, file);
   const url = await getDownloadURL(storageRef);
+
+  // Ajouter dans Firestore (timestamp si tu veux plus tard)
+  await addDoc(collection(db, "snaps"), {
+    userId,
+    imageUrl: url,
+    timestamp: Date.now()
+  });
+
   return url;
 }
