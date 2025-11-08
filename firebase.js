a0ba497b3d94e15ab0ade8d49d2e8ae9be49bbd6
hoobasurf <brice.paneela@icloud.com>
2025-11-08 21:18:48 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
 // firebase.js
-// Import Firebase SDK depuis le CDN
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
-// Tu pourras ajouter d'autres services Firebase si nécessaire
-// Exemple: import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
-// Configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCVl5ktUmuiVeMtYUHtxORytkeu71_XBF0",
   authDomain: "changefamilysnap.firebaseapp.com",
@@ -16,9 +12,7 @@ const firebaseConfig = {
   measurementId: "G-8D5PC0EZ74"
 };
 
-// Initialise Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Export pour pouvoir utiliser Firebase dans d'autres fichiers
 export { app, analytics };
