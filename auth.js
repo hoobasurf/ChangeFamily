5edf2cfd99ad3747010ba4e92a68ec5cdbc000eb
hoobasurf <brice.paneela@icloud.com>
2025-11-08 22:14:13 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,28 +1,16 @@
-// auth.js
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } 
-  from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
-import { app } from './firebase.js';
+import { auth } from './firebase.js';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
-const auth = getAuth(app);
-
-// Création utilisateur
-export function signup(email, password) {
-  return createUserWithEmailAndPassword(auth, email, password);
+async function signup(email, password) {
+  return await createUserWithEmailAndPassword(auth, email, password);
 }
 
-// Connexion utilisateur
-export function login(email, password) {
-  return signInWithEmailAndPassword(auth, email, password);
+async function login(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
 }
 
-// Déconnexion
-export function logout() {
-  return signOut(auth);
+async function logout() {
+  return await signOut(auth);
 }
 
-// Observer l'état de connexion
-export function onAuthStateChanged(callback) {
-  return auth.onAuthStateChanged(callback);
-}
-
-export { auth };
+export { signup, login, logout, auth, onAuthStateChanged };
